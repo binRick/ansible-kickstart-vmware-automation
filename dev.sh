6777967c141a6fbc8907e9259c16f2967827a4be
Richard Blundell <rblundell@deloitte.com>
2019-05-24 14:22:47 -0400

    remove silly component

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-nodemon -w . -e yaml,j2,txt -x ansible-playbook -- -i Inventory.txt playbooks/setup.yaml
+nodemon -w . -e yaml,j2,txt -x ansible-playbook -- -i Inventory.txt playbooks/esxHosts.yaml
+#nodemon -w . -e yaml,j2,txt -x ansible-playbook -- -i Inventory.txt playbooks/setup.yaml
 #nodemon -w . -e yaml,j2,txt -x ansible-playbook -- -i Inventory.txt playbooks/generateVmKickstartFile.yaml
