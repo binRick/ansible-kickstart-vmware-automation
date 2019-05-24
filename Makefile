2e3bfef335c2eb94c807a9d9678c8aea3e9af1b9
root <admin@vpntech.net>
2019-05-24 07:50:33 -0400

    code sync

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build/%.iso: build/%.cfg
 	./build-configuration-iso.sh build/$*.cfg build/$*.iso
 
 build/%.cfg: kickstart.cfg.j2
-	ansible-playbook --limit $* template-kickstart.yml
+	ansible-playbook -i Inventory.txt -l $* playbooks/install.yaml
 
 .PHONY: clean
 clean:
--- dev.sh
+++ dev.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nodemon -w . -e yaml,j2,txt -x ansible-playbook -- -i Inventory.txt playbooks/install.yaml 
+nodemon -w . -e yaml,j2,txt -x ansible-playbook -- -i Inventory.txt playbooks/setup.yaml
+#nodemon -w . -e yaml,j2,txt -x ansible-playbook -- -i Inventory.txt playbooks/generateVmKickstartFile.yaml
